a4baea792180c27600fb67168c9f6f39e6e5bce5
Chloe ONeil <chloesporri@yahoo.com>
2022-11-28 19:47:43 -0800

    add age, movie and time functions Co-Authored-By Noah Cowan <nsncowan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,45 @@ this.age = age;
 this.movie = movie;
 }
 
-Ticket.prototype.addPrice = function() {
-  let price = 0;
-  if (time === "matinee") {
-    price += 5;
+Ticket.prototype.timePrice = function() {
+  let timePrice = 0;
+  if (Ticket.time === "matinee") {
+    timePrice += 5;
   }
-  else if (time === "afternoon") {
-    price += 10;
+  else if (Ticket.time === "afternoon") {
+    timePrice += 10;
   }
   else {
-    price += 15
+    timePrice += 15;
   }
+  return timePrice;
+}
+
+Ticket.prototype.agePrice = function() {
+  let agePrice = 0;
+  if (Ticket.age === "youth") {
+    timePrice += 2;
+  }
+  else if (Ticket.age === "senior") {
+    agePrice += 5;
+  }
+  else {
+    agePrice += 10;
+  }
+  return agePrice;
+}
+
+Ticket.prototype.moviePrice = function() {
+  let moviePrice = 0;
+  if (Ticket.movie === "old movie") {
+    moviePrice += 2;
+  }
+  else {
+    moviePrice += 5;
+  }
+  return moviePrice;
+}
+
+function totalPrice() {
+  return (timePrice + agePrice + moviePrice).toString();
 }
